5c9bc72f09348a8c70070e640d6d12dc4a63e84d
Thomas Rickard <thomas_rickard@yahoo.co.uk>
2016-09-10 00:42:12 +0100

    added arduino firmware

---
+++ temp-sensor-firmware/temp-sensor-firmware.ino
@@ -0,0 +1,61 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+  
+  // Start up the temp sensor library
+  sensors.begin();
+}
+
+void loop() {
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    if(inputString == "temp\n") {
+      // Get temperature
+      sensors.requestTemperatures();
+      Serial.println(sensors.getTempCByIndex(0));
+    }
+    inputString = "";
+    stringComplete = false;
+  }
+//  Serial.println(inputString);
+//  delay(1000);
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
+}
+
+
